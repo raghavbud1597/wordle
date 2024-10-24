d703450db645646a4c18566e494d4f84f40899e3
raghavbud1597 <raghav1597@gmail.com>
2024-10-24 19:28:26 -0400

    test cases for components

---
+++ app/__tests__/WordleCellComponent.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import '@testing-library/jest-dom';
+import WordleCell from './../components/WordleCell';
+
+describe('WordleCell Component', () => {
+  it('renders the letter and applies correct styles based on score', () => {
+    const { rerender } = render(<WordleCell letter="A" score={0} />);
+    
+    expect(screen.getByText('A')).toBeInTheDocument();
+    expect(screen.getByText('A').parentElement).toHaveClass('bg-slate-500');
+    
+    rerender(<WordleCell letter="B" score={1} />);
+    expect(screen.getByText('B')).toBeInTheDocument();
+    expect(screen.getByText('B').parentElement).toHaveClass('bg-yellow-600');
+    
+    rerender(<WordleCell letter="C" score={2} />);
+    expect(screen.getByText('C')).toBeInTheDocument();
+    expect(screen.getByText('C').parentElement).toHaveClass('bg-green-600');
+  });
+});
